1f8b3a43462d4d0bfdfcfaf731a33211c8ba7564
Airen <weihua.lwh@alibaba-inc.com>
2018-03-21 15:12:34 +0800

    add notched box demo

---
+++ vue-notch-box/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <div class="form">
+      <input type="range" min="0" max="50" v-model="notchSize" @input="change">
+    </div>
+    <notch-box :child-notch-size = notchSize ></notch-box>
+  </div>
+</template>
+
+<script>
+import NotchBox from './components/NotchBox'
+
+export default {
+  name: 'App',
+  components: {
+    NotchBox
+  },
+  data () {
+    return {
+      notchSize: '20'
+    }
+  },
+  methods: {
+    change: function (e) {
+      this.notchSize = e.target.value
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Lato|Roboto:700');
+
+body {
+  background-color: #777;
+  background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAQAAABuBnYAAAAAU0lEQVQIHQXBwRGDIBAAwO2/AMcCDHAcPjIRxjJ8Je1kl1uqUgphsWu6w0sIG6npLpcUBql4e/wsVRKabrkNTacIYbMrwsF06rqUhsnXVKVT+Hj+Ue4rPSONk4kAAAAASUVORK5CYII=);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-family: 'Lato', sans-serif;
+  line-height: 1.4;
+  color: #ffdebd;
+  margin: 1rem;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h2 {
+  color: white;
+  margin: 0 0 1rem 0;
+  font-family: 'Roboto', sans-serif;
+}
+p {
+  margin: 0;
+}
+
+input {
+  position: absolute;
+  top: 20px;
+  left: 5vw;
+  width: 90vw;
+}
+</style>
---
+++ vue-notch-box/src/components/NotchBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="notch" :style="{'--notchSize': changeChildNotchSize}">
+    <slot>This is notch module box!</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotchBox',
+  props: ['childNotchSize'],
+  computed: {
+    changeChildNotchSize: function () {
+      return this.childNotchSize + 'px'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.notch {
+  max-width: 400px;
+  padding: 3rem;
+  background: #f06d06;
+  
+  --notchSize: 20px;
+  
+  clip-path: 
+    polygon(
+      0% var(--notchSize), 
+      var(--notchSize) 0%, 
+      calc(100% - var(--notchSize)) 0%, 
+      100% var(--notchSize), 
+      100% calc(100% - var(--notchSize)), 
+      calc(100% - var(--notchSize)) 100%, 
+      var(--notchSize) 100%, 
+      0% calc(100% - var(--notchSize))
+    );
+}
+
+</style>
